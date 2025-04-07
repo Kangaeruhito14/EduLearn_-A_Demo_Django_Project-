e62f81a100407a3590427f0e1b29eb56917d4d5b
Kangaeruhito14 <ap1484480@gmail.com>
2025-04-07 23:07:26 +0600

    Update commit-all-edit-34

--- eduLearn/urls.py
+++ eduLearn/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from courses import views
+from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('courses/', include('courses.urls')),
 ]
 
