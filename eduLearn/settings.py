8b3459b323c626f23ab5da35ae985865bf9be2b5
Kangaeruhito14 <ap1484480@gmail.com>
2025-04-08 20:34:43 +0600

    Update commit-all-edit-51

--- eduLearn/settings.py
+++ eduLearn/settings.py
@@ -152,8 +152,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
